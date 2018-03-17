9175fbaea30d536acc3b7cc03f82cbe62cc939ca
‘Carlos <Bo@dhcp-wifi-8021x-155-41-121-236.bu.edu>
2018-03-17 03:41:50 -0400

    Maps of locations in Bude

---
+++ Bude.R
@@ -0,0 +1,87 @@
+library(tidyverse)
+library(ggmap)
+
+
+# Empty maps
+
+## Road map
+map_road <- get_googlemap(center = c(-4.543678, 50.826636), zoom = 14)
+ggmap(map_road)
+
+## Watercolor map
+map_wtr <- get_map("Bude", maptype = "watercolor",zoom= 14)
+ggmap(map_wtr)
+
+# Marked maps
+
+## Road map
+ggmap(map_road) +
+  geom_point(
+    aes(x = -4.55134929999997 , y = 50.8305399),
+    color = "red", size = 3) +
+  geom_point(
+    aes(x = -4.553962 , y = 50.83587),
+    color = "blue", size = 3) +
+  geom_point(
+    aes(x = -4.550984200000016 , y = 50.8361969),
+    color = "orange", size = 3)+
+  geom_point(
+    aes(x = -4.552814, y = 50.83347),
+    color = "yellow", size = 3)
+
+## Watercolor map
+ggmap(map_wtr) +
+  geom_point(
+    aes(x = -4.55134929999997 , y = 50.8305399),
+    color = "red", size = 3) +
+  geom_point(
+    aes(x = -4.553962 , y = 50.83587),
+    color = "blue", size = 3) +
+  geom_point(
+    aes(x = -4.550984200000016 , y = 50.8361969),
+    color = "orange", size = 3)+
+  geom_point(
+    aes(x = -4.552814, y = 50.83347),
+    color = "yellow", size = 3)
+
+# Maps with routes
+
+from <- "Bude North Cornwall Cricket Club"
+to <- "Crooklets Inn"
+route_df <- route(from, to, structure = "route")
+
+## Road map
+map_road <- get_map("Bude North Cornwall Cricket Club", maptype = "roadmap",zoom= 16)
+
+ggmap(map_road) +
+  geom_point(
+    aes(x = -4.550984200000016 , y = 50.8361969),
+    color = "orange", size = 3)+
+  geom_point(
+    aes(x = -4.552814, y = 50.83347),
+    color = "yellow", size = 3) +
+  geom_path(
+    aes(x = lon, y = lat), colour = "red", size = 1.5,
+    data = route_df, lineend = "round"
+  )
+
+
+## Watercolor map
+map_road <- get_map("Bude North Cornwall Cricket Club", maptype = "watercolor",zoom= 16)
+
+ggmap(map_wtr) +
+  geom_point(
+    aes(x = -4.550984200000016 , y = 50.8361969),
+    color = "orange", size = 3)+
+  geom_point(
+    aes(x = -4.552814, y = 50.83347),
+    color = "yellow", size = 3) +
+  geom_path(
+    aes(x = lon, y = lat), colour = "red", size = 1.5,
+    data = route_df, lineend = "round"
+  )
+
+
+
+
+             
